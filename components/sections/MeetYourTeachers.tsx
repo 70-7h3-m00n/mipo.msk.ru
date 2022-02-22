9955818f8ae5dee089631a5f0eb62f5f96678170
Novailoveyou <novailoveyoux@gmail.com>
2022-02-22 13:23:02 +0300

    added numeric_order filtering for teachers in MeetYourTeachers

--- components/sections/MeetYourTeachers.tsx
+++ components/sections/MeetYourTeachers.tsx
@@ -6,33 +6,42 @@ import PopupTrigger from '@/components/general/PopupTrigger'
 import { ImgTeacher } from '@/components/imgs'
 
 const MeetYourTeachers = ({ teachers }) => {
+  console.log(teachers)
   return (
     <section className={stls.container}>
       <Wrapper>
         <h1 className={stls.title}>Познакомьтесь с вашими наставниками</h1>
         <ul className={stls.teachers}>
           {teachers &&
-            teachers.map(teacher => (
-              <li key={teacher?.name} className={stls.teacher}>
-                <div className={stls.img}>
-                  <ImgTeacher
-                    src={teacher?.portrait?.url}
-                    alt={teacher?.name}
-                    width={teacher?.portrait?.width && 270}
-                    height={getImageHeight({
-                      width: 270,
-                      widthInitial: teacher?.portrait?.width,
-                      heightInitial: teacher?.portrait?.height
-                    })}
-                    atMeetYourTeachers
-                  />
-                </div>
-                <div>
-                  <div className={stls.name}>{teacher?.name}</div>
-                  <div className={stls.bio}>{parse(teacher?.achievements)}</div>
-                </div>
-              </li>
-            ))}
+            teachers
+              .sort(
+                (a, b) =>
+                  Number(a?.numeric_order?.idx || Infinity) -
+                  Number(b?.numeric_order?.idx || Infinity)
+              )
+              .map(teacher => (
+                <li key={teacher?.name} className={stls.teacher}>
+                  <div className={stls.img}>
+                    <ImgTeacher
+                      src={teacher?.portrait?.url}
+                      alt={teacher?.name}
+                      width={teacher?.portrait?.width && 270}
+                      height={getImageHeight({
+                        width: 270,
+                        widthInitial: teacher?.portrait?.width,
+                        heightInitial: teacher?.portrait?.height
+                      })}
+                      atMeetYourTeachers
+                    />
+                  </div>
+                  <div>
+                    <div className={stls.name}>{teacher?.name}</div>
+                    <div className={stls.bio}>
+                      {parse(teacher?.achievements)}
+                    </div>
+                  </div>
+                </li>
+              ))}
         </ul>
         <div className={stls.btn}>
           <PopupTrigger btn='alpha' cta='getFullList' />
