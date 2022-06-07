ae7ff09d3fbc3ab7dcbd2365804bf6e8d99569bf
novailoveyou <novailoveyoux@gmail.com>
2022-06-07 15:24:25 +0300

    added more of tinkoff payment

--- components/sections/payment/SectionPaymentTinkoff.tsx
+++ components/sections/payment/SectionPaymentTinkoff.tsx
@@ -1,82 +1,125 @@
 import stls from '@/styles/components/sections/payment/SectionPaymentTinkoff.module.sass'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import cn from 'classnames'
 import Wrapper from '@/components/layout/Wrapper'
-import { BtnAlpha } from '@/components/btns'
+import { BtnAlpha, BtnBeta, BtnDelta, BtnGamma } from '@/components/btns'
 import PopupTrigger from '@/components/general/PopupTrigger'
 
 const SectionPaymentTinkoff = () => {
+  const router = useRouter()
+
   const handleSubmit = e => {
-    pay(this)
-    return false
+    try {
+      // @ts-ignore
+      pay(e)
+      return false
+    } catch (err) {
+      console.log(err)
+    }
   }
   return (
-    <section className={stls.container}>
-      <Wrapper>
-        <h2 className={stls.title}>Оплата Тинькофф</h2>
-        <Script src='https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js' />
-        <form
-          name='TinkoffPayForm'
-          onSubmit={handleSubmit}
-          className={stls.form}>
-          <input
-            className={stls.input}
-            type='hidden'
-            name='terminalkey'
-            value='TinkoffBankTest'
-          />
-          <input
-            className={stls.input}
-            type='hidden'
-            name='frame'
-            value='true'
-          />
-          <input
-            className={stls.input}
-            type='hidden'
-            name='language'
-            value='ru'
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='Сумма заказа'
-            name='amount'
-            required
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='Номер заказа'
-            name='order'
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='Описание заказа'
-            name='description'
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='ФИО плательщика'
-            name='name'
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='E-mail'
-            name='email'
-          />
-          <input
-            className={stls.input}
-            type='text'
-            placeholder='Контактный телефон'
-            name='phone'
-          />
-          <input className={stls.submitBtn} type='submit' value='Оплатить' />
-        </form>
-      </Wrapper>
-    </section>
+    <>
+      <Script src='https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js' />
+      <section className={stls.container}>
+        <Wrapper>
+          <h2 className={stls.title}>Оплата Тинькофф</h2>
+          <form
+            name='TinkoffPayForm'
+            onSubmit={handleSubmit}
+            className={stls.form}>
+            <div className={stls.hiddenInputs}>
+              <input
+                className='tinkoffPayRow'
+                type='hidden'
+                name='terminalkey'
+                value='1650440757930DEMO'
+              />
+              {/* <input
+                className='tinkoffPayRow'
+                type='hidden'
+                name='reccurentPayment'
+                value='true'
+              />
+              <input
+                className='tinkoffPayRow'
+                type='hidden'
+                name='customerKey'
+                value=''
+              /> */}
+              <input
+                className='tinkoffPayRow'
+                type='hidden'
+                name='frame'
+                value='false'
+              />
+              <input
+                className='tinkoffPayRow'
+                type='hidden'
+                name='language'
+                value='ru'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='Сумма заказа'
+                name='amount'
+                required
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='Номер заказа'
+                name='order'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='Описание заказа'
+                name='description'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='ФИО плательщика'
+                name='name'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='E-mail'
+                name='email'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.input, 'tinkoffPayRow')}
+                type='text'
+                placeholder='Контактный телефон'
+                name='phone'
+              />
+            </div>
+            <div className={stls.inputGroup}>
+              <input
+                className={cn(stls.submitBtn, 'tinkoffPayRow')}
+                type='submit'
+                value='Оплатить'
+              />
+            </div>
+          </form>
+        </Wrapper>
+      </section>
+    </>
   )
 }
 
