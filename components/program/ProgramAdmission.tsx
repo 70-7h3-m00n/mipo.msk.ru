c7548e78a76e2cc2eea9ef8cbb99d280f0fb3457
novailoveyou <novailoveyoux@gmail.com>
2022-11-22 06:46:10 +0300

    added closestAdmission data fetching from the api

--- components/program/ProgramAdmission.tsx
+++ components/program/ProgramAdmission.tsx
@@ -1,5 +1,15 @@
 import { calculateClosestAdmission } from '@/helpers/index'
+import ProgramContext from '@/context/program/programContext'
+import { useContext } from 'react'
 
-const ProgramAdmission = () => calculateClosestAdmission()
+const ProgramAdmission = () => {
+  const { program } = useContext(ProgramContext)
+
+  const closestAdmission = program?.timenprice?.[0]?.closestAdmission
+
+  if (closestAdmission) return closestAdmission
+
+  return calculateClosestAdmission()
+}
 
 export default ProgramAdmission
