87255fafc8099f2fef8f82c94e48a9987dfedd6d
Zhenya <asgyrdjons@gmail.com>
2023-09-06 19:25:48 +0300

    feature(eddu.pro): added block eddu.pro

---
+++ data/eddu/index.ts
@@ -0,0 +1,21 @@
+import svg1 from './eddu/eddu-counter-white-simple49.svg'
+import svg2 from './eddu/eddu-counter-white-simple48.svg'
+import svg3 from './eddu/eddu-counter-white-simple47.svg'
+
+type EdduData = {
+  svg : string
+}
+
+const edduData : EdduData[] = [
+  {
+    svg : svg1
+  },
+  {
+    svg : svg2
+  },
+  {
+    svg : svg3
+  }
+]
+
+export default edduData
