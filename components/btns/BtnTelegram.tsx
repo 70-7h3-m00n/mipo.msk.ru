067945148a4ef066a7ede429987fed04d86649c7
Evgeny <asgyrdjons@gmail.com>
2024-01-16 10:38:40 +0300

    feat(footer): Добавление ссылки на телеграм канал

--- components/btns/BtnTelegram.tsx
+++ components/btns/BtnTelegram.tsx
@@ -1,10 +1,13 @@
 import stls from '@/styles/components/btns/BtnTelegram.module.sass'
 import { IconTelegram } from '@/components/icons'
 import classNames from 'classnames'
+import { useRouter } from 'next/router'
 
 const BtnTelegram = ({ dark = false }) => {
+  const router = useRouter()
   return (
     <button
+      onClick={() => router.push('https://t.me/mipomsk')}
       className={classNames({
         [stls.container]: true,
         [stls.dark]: dark
