211a4c9aeafc1136ba8c2010f6054c1b485bd701
AsgurDjons <asgyrdjons@gmail.com>
2024-02-21 16:10:26 +0300

    fix: Добавление даты 30 и 29 часла зачислений

--- helpers/calculateClosestAdmission.ts
+++ helpers/calculateClosestAdmission.ts
@@ -1,19 +1,28 @@
-const setLastDayOfMonth = (currentDate: Date) => {
-  currentDate.setMonth(currentDate.getMonth() + 1)
-  currentDate.setDate(10)
-  // currentDate.setMonth(currentDate.getMonth() - 1)
+function getLastDayOfMonth(currentDate: Date) {
+  // Получаем месяц текущей даты
+  let currentMonth = currentDate.getMonth();
+
+  // Устанавливаем дату на первый день следующего месяца
+  currentDate.setMonth(currentMonth + 1, 1);
+
+  // Затем вычитаем один день, чтобы получить последний день текущего месяца
+  currentDate.setDate(currentDate.getDate() - 1);
+
+  if (currentDate.getDate() === 31) {
+    return currentDate.getDate() - 1
+  } else {
+    return currentDate.getDate();
+  }
 }
 
 const setNextDay = (currentDate: Date, currentDay: number) =>
   currentDay <= 20
     ? currentDate.setDate(currentDay <= 10 ? 10 : 20)
-    : setLastDayOfMonth(currentDate)
+    : getLastDayOfMonth(currentDate)
 
 const calculateClosestAdmission = () => {
   const currentDate = new Date()
-
   setNextDay(currentDate, currentDate.getDate())
-  console.log(currentDate.toLocaleString('ru-RU', { day: 'numeric', month: 'long' }))
   return currentDate.toLocaleString('ru-RU', { day: 'numeric', month: 'long' })
 }
 
