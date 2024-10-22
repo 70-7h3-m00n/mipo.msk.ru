8782c41a7b27b1817489454f3a900bc652d209dc
AirPC2 <i@memelu.ru>
2024-10-22 18:59:47 +0300

    Добавить sale блок на страницы сайта

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -22,6 +22,7 @@ import BtnPhone from '@/components/btns/BtnPhone'
 import BtnHumburger from '@/components/btns/BtnHumburger'
 import { useRouter } from 'next/router'
 import colors from '@/config/colors'
+import SalesBlockToHeader from './SalesBlockToHeader'
 
 const Header = () => {
   const router = useRouter()
@@ -55,13 +56,73 @@ const Header = () => {
   const redirectHeader = router.asPath.includes('new-courses')
 
   return (
-      <header className={cn( !redirectHeader? stls.container: stls.contentNewCourse, { [stls.altStyles]: altStyles })}>
-        <MenuMobile />
+    <header
+      className={cn(!redirectHeader ? stls.container : stls.contentNewCourse, {
+        [stls.altStyles]: altStyles
+      })}>
+      
+      <SalesBlockToHeader />
+      <MenuMobile />
 
-        {
-          !redirectHeader ?
+      {!redirectHeader ? (
+        <Wrapper>
+          <div className={cn(stls.top, { [stls.altStyles]: altStyles })}>
+            <div className={stls.topleft}>
+              <Link href={routeLegal}>
+                <a className={stls.linkInfo}>
+                  Сведения об образовательной организации
+                </a>
+              </Link>
+              <div className={stls.location}>
+                <div className={stls.icon}>
+                  <IconLocation />
+                </div>
+                <p className={stls.p}>
+                  {city}, {street}
+                </p>
+              </div>
+            </div>
+            <div className={stls.topright}>
+              <div className={stls.phone}>
+                <BtnPhone withNumber />
+              </div>
+              <div className={stls.phoneNoNum}>
+                <BtnPhone />
+              </div>
+              <PopupTrigger btn='epsilon' cta='callMeBack' />
+            </div>
+          </div>
+
+          <div className={stls.row}>
+            <Logo atHeader />
+            <div className={stls.btns}>
+              <BtnPhone />
+              <BtnHumburger />
+            </div>
+            <div className={stls.btnFields}>
+              <BtnFields />
+            </div>
+            {list.map((item, idx) => (
+              <Link key={item.href + item.val} href={item.href}>
+                <a
+                  className={cn([stls.link], {
+                    [stls.linkFirst]: idx === 0,
+                    [stls.linkThird]: idx === 2
+                  })}>
+                  {item.val}
+                </a>
+              </Link>
+            ))}
+          </div>
+        </Wrapper>
+      ) : (
+        <>
+          <div
+            style={{
+              background: '#17192C'
+            }}>
             <Wrapper>
-              <div className={cn(stls.top, { [stls.altStyles]: altStyles })}>
+              <div className={cn(stls.top)}>
                 <div className={stls.topleft}>
                   <Link href={routeLegal}>
                     <a className={stls.linkInfo}>
@@ -87,97 +148,45 @@ const Header = () => {
                   <PopupTrigger btn='epsilon' cta='callMeBack' />
                 </div>
               </div>
+            </Wrapper>
+          </div>
 
-              <div className={stls.row}>
+          <div className={stls.mobileCourse}>
+            <Wrapper>
+              <div
+                className={stls.row}
+                style={{
+                  padding: '8px 0'
+                }}>
                 <Logo atHeader />
+
                 <div className={stls.btns}>
                   <BtnPhone />
                   <BtnHumburger />
                 </div>
+
                 <div className={stls.btnFields}>
                   <BtnFields />
                 </div>
+
                 {list.map((item, idx) => (
                   <Link key={item.href + item.val} href={item.href}>
                     <a
                       className={cn([stls.link], {
                         [stls.linkFirst]: idx === 0,
                         [stls.linkThird]: idx === 2
-                      })}>
+                      })}
+                      style={{ color: 'black' }}>
                       {item.val}
                     </a>
                   </Link>
                 ))}
               </div>
             </Wrapper>
-            :
-            <>
-              <div style={{
-                background: '#17192C'
-              }}>
-                <Wrapper>
-                  <div className={cn(stls.top)}>
-                    <div className={stls.topleft}>
-                      <Link href={routeLegal}>
-                        <a className={stls.linkInfo}>
-                          Сведения об образовательной организации
-                        </a>
-                      </Link>
-                      <div className={stls.location}>
-                        <div className={stls.icon}>
-                          <IconLocation />
-                        </div>
-                        <p className={stls.p}>
-                          {city}, {street}
-                        </p>
-                      </div>
-                    </div>
-                    <div className={stls.topright}>
-                      <div className={stls.phone}>
-                        <BtnPhone withNumber />
-                      </div>
-                      <div className={stls.phoneNoNum}>
-                        <BtnPhone />
-                      </div>
-                      <PopupTrigger btn='epsilon' cta='callMeBack' />
-                    </div>
-                  </div>
-                </Wrapper>
-              </div>
-
-              <div className={stls.mobileCourse}>
-                <Wrapper>
-                  <div className={stls.row} style={{
-                    padding: '8px 0'
-                  }}>
-                    <Logo atHeader />
-
-                    <div className={stls.btns}>
-                      <BtnPhone />
-                      <BtnHumburger />
-                    </div>
-
-                    <div className={stls.btnFields}>
-                      <BtnFields />
-                    </div>
-
-                    {list.map((item, idx) => (
-                      <Link key={item.href + item.val} href={item.href}>
-                        <a
-                          className={cn([stls.link], {
-                            [stls.linkFirst]: idx === 0,
-                            [stls.linkThird]: idx === 2
-                          })} style={{ color: 'black' }}>
-                          {item.val}
-                        </a>
-                      </Link>
-                    ))}
-                  </div>
-                </Wrapper>
-              </div>
-            </>
-        }
-      </header>
+          </div>
+        </>
+      )}
+    </header>
   )
 }
 
